13de320097b90327802672e6147071866ec1bf3e
iitenkida7 <t.ishii@teradox.net>
2023-02-22 17:48:11 +0900

    Remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 //import { useState } from "react";
 //import reactLogo from "./assets/react.svg";
-import Hello  from "./Hello";
+import Hello from "./Hello";
 import { createCards } from "./Cards";
 import "./App.css";
 
 function App() {
-
-  type Item = {
-    id: number;
-    title: string;
-  };
   type Card = {
     color: string;
     number: number;
   };
-
-  const items: Item[] = [
-    { id: 1, title: "yamada" },
-    { id: 2, title: "tokashiki" },
-    { id: 3, title: "ohtsubo" },
-    { id: 4, title: "ishii" },
-  ];
-
 
   const cards = createCards();
 
@@ -30,14 +17,14 @@ function App() {
       <ul>
         {cards.map((card: Card) => (
           <li>
-            <span className="text-3xl font-bold underline">{card.color} / {card.number}</span>
+            <span className="text-3xl font-bold underline">
+              {card.color} / {card.number}
+            </span>
           </li>
         ))}
       </ul>
-      <Hello />
     </div>
   );
 }
-
 
 export default App;
