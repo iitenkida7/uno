9f6af29a4c98848c00a7cff99338b4ed87cd3bb2
iitenkida7 <t.ishii@teradox.net>
2023-02-22 17:28:30 +0900

    Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+//import { useState } from "react";
+//import reactLogo from "./assets/react.svg";
+import Hello  from "./Hello";
+import { createCards } from "./Cards";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  type Item = {
+    id: number;
+    title: string;
+  };
+
+  const items: Item[] = [
+    { id: 1, title: "yamada" },
+    { id: 2, title: "tokashiki" },
+    { id: 3, title: "ohtsubo" },
+    { id: 4, title: "ishii" },
+  ];
+
+
+  const cards = createCards();
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {items.map((item: Item) => (
+          <li>
+            {item.id} / {item.title}
+          </li>
+        ))}
+      </ul>
+      <Hello />
     </div>
-  )
+  );
 }
 
-export default App
+
+export default App;
---
+++ src/Hello.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hello: React.FC = () => {
+  const message: string = "Hello React!";
+
+  return <div>{message}</div>;
+};
+
+export default Hello;
